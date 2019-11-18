0e8b52a6cd59df7a1cd9b2a31b88d8e96bf2de2b
KMY <muziq43@hanafn.com>
2019-11-18 09:17:41 +0900

    Initial Commit

---
+++ 002Scene-ManualSegue02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  002Scene-ManualSegue02
+//
+//  Created by 김민영 on 11/5/19.
+//  Copyright © 2019 김민영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
